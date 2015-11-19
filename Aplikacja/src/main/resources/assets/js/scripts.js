dd03c870954b3c00780b10727236e780449204c2
Kombi92 <92.konrad@gmail.com>
2015-11-19 11:10:06 +0100

    Cookies

--- Aplikacja/src/main/resources/assets/js/scripts.js
+++ Aplikacja/src/main/resources/assets/js/scripts.js
@@ -9,9 +9,39 @@ if (nickValue == null) {
 function nickChange() {		
 				//var nickValue = document.getElementById('Nick').value
 				window.location = "/?name="+nickValue;
-*/
+
 if (document.URL.indexOf("?name") <= 0) {
 	nickValue = null 
 	nickValue = window.prompt("Type your nick here.","Your Nick");
     window.location = "/?name="+nickValue;
+}
+*/
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
+    }
 }
