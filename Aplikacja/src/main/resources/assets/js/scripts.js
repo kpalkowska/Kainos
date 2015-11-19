0c69c5a4d6142699900e04a5f2f65e81e3b9bf9d
Kombi92 <92.konrad@gmail.com>
2015-11-19 10:28:07 +0100

    Nick change

--- Aplikacja/src/main/resources/assets/js/scripts.js
+++ Aplikacja/src/main/resources/assets/js/scripts.js
@@ -1,8 +1,17 @@
-			function nickChange() {
-				
-				
-				var nickValue = document.getElementById('Nick').value
+/*
+window.alert('hey!');
+
+
+if (nickValue == null) {
+	
+}
+
+function nickChange() {		
+				//var nickValue = document.getElementById('Nick').value
 				window.location = "/?name="+nickValue;
-				
-			}
-			
+*/
+if (document.URL.indexOf("?name") <= 0) {
+	nickValue = null 
+	nickValue = window.prompt("Type your nick here.","Your Nick");
+    window.location = "/?name="+nickValue;
+}
