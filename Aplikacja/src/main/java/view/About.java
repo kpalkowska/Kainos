6c7b3919d058c0dd6e6cd3e04d7bf44b113d64da
kpalkowska <kpalkowska@sigma.ug.edu.pl>
2015-11-19 13:07:30 +0100

    update About.java

--- Aplikacja/src/main/java/view/About.java
+++ Aplikacja/src/main/java/view/About.java
@@ -1,19 +1,7 @@
 package view;
 
 public class About {
-	
-	/*private Person person;
-	public String name;
-	
-    public String getName() {
-        return person.getName();
-    }
-    
-    public About(String name){
-        this.name = person.getName();
-    }*/
-    
-    
+
     public String name;
 	
     public String getName() {
